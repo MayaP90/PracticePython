c5b1ad5dfa9ecd1312919e2cb5865aebaa7b5659
MayaP90 <maya@Mayas-Air.fios-router.home>
2021-03-08 12:46:09 -0500

    E1 written

---
+++ E1.py
@@ -0,0 +1,13 @@
+# This is the first python code using input
+
+# print is a reserved word
+
+# This is the first exercise: Create a program that asks the user to enter their name
+# and their age. Print out a message addressed to them that tells them the year
+# that they will turn 100 years old.
+
+name = input("Enter your name:")
+age = int(input("How old are you"))
+year = str((2021 - age) + 100)
+
+print(name + " will be 100 years old in the year " + year)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# PracticePython
-A place where I will be putting and working through my python practice problems.
+# Practice Python
+
+A place where I will be putting and working through my python practice problems. There will be a folder for each website that I will use
+
+Some of the sources and websites that I will be using are included here. 
+    Python Practice 
+        Website Link: 
